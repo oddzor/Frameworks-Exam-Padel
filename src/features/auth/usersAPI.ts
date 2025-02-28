f4d8276e0c5f92deaea68d62dc9c7e3a007b68a4
oddzor <odd.grimholt@gokstadakademiet.no>
2025-02-28 02:15:16 +0100

    removed some leftover files and functions from previous functionality

--- src/features/auth/usersAPI.ts
+++ src/features/auth/usersAPI.ts
@@ -15,14 +15,6 @@ export async function getAllUsers(): Promise<User[]> {
   return response.json();
 }
 
-export async function getUser(userId: string): Promise<User> {
-  const response = await fetch(`${USERS_ENDPOINT}/${userId}`);
-  if (!response.ok) {
-    throw new Error("Failed to fetch that user");
-  }
-  return response.json();
-}
-
 export async function registerUser(userData: Omit<User, "_id">): Promise<User> {
   const response = await fetch(USERS_ENDPOINT, {
     method: "POST",
@@ -35,27 +27,3 @@ export async function registerUser(userData: Omit<User, "_id">): Promise<User> {
   return response.json();
 }
 
-export async function updateUser(
-  userId: string,
-  updatedData: Partial<User>
-): Promise<User> {
-  const response = await fetch(`${USERS_ENDPOINT}/${userId}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(updatedData),
-  });
-  if (!response.ok) {
-    throw new Error("Failed to update user");
-  }
-  return { ...(updatedData as User), _id: userId };
-}
-
-export async function deleteUser(userId: string): Promise<string> {
-  const response = await fetch(`${USERS_ENDPOINT}/${userId}`, {
-    method: "DELETE",
-  });
-  if (!response.ok) {
-    throw new Error("Failed to delete user");
-  }
-  return userId;
-}
